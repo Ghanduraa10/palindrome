125437f891ebf8a2e08c9075ebf3863e657d2146
Ghandur <ghandurathallah10@gmail.com>
2023-05-15 20:53:12 +0700

    feat : logic palindrome

---
+++ Tes Logic PT.RAS/index.js
@@ -0,0 +1,15 @@
+function isPalindrome(word) {
+  let cleanWord = word.toLowerCase();
+  let reversedWord = "";
+  for (let i = cleanWord.length - 1; i >= 0; i--) {
+    reversedWord += cleanWord[i];
+  }
+  return cleanWord === reversedWord;
+}
+
+let word = "Main"
+if (isPalindrome(word)) {
+  console.log(`"${word}" adalah palindrome `);
+} else {
+  console.log(`"${word}" bukan palindrome `);
+}
